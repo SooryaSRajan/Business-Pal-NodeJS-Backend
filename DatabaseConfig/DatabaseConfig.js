940a7b68e0163c02382b3ba80d2bcb21f6389700
sooryasrajan <soorya.s27@gmail.com>
2022-01-10 01:35:28 +0530

    Changed database config

--- DatabaseConfig/DatabaseConfig.js
+++ DatabaseConfig/DatabaseConfig.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 function ConnectMongoDB() {
     let connection = mongoose.connect(
         //'mongodb://127.0.0.1:27017/local-business-db'
-        process.env.MONGO_CLIENT
+        //process.env.MONGO_CLIENT
+        "mongodb+srv://soorya:suvanika2707@cluster0.hdnla.mongodb.net/business_pal"
     );
 
     connection.then(() => {
